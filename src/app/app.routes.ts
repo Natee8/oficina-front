8dcf7cfded00ecf4f3d827d6d5006ddae40cdfa7
Nathália Almeida Amorum <nathaliaalmeida@prefeitura.sp.gov.br>
2026-03-12 11:12:41 -0300

    style: definindo estilos globais

---
+++ src/app/app.routes.ts
@@ -0,0 +1,9 @@
+import { Routes } from '@angular/router';
+import {RegisterComponent } from '../app/features/auth/register/pages/register.component'
+
+export const routes: Routes = [
+  {
+    path: 'register',
+    component: RegisterComponent
+  }
+];
