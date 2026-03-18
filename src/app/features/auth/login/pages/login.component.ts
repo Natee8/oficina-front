9c341f523882964a8bbb41277d211912bbed5287
Nathália Almeida Amorum <nathaliaalmeida@prefeitura.sp.gov.br>
2026-03-18 11:08:59 -0300

    commit

--- src/app/features/auth/login/pages/login.component.ts
+++ src/app/features/auth/login/pages/login.component.ts
@@ -9,12 +9,11 @@ import { ToggleActionsComponent } from '../../../../shared/components/buttonNext
   standalone: true,
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss'],
-  imports: [InputFieldComponent, RouterLink, BackButtonComponent, ToggleActionsComponent],
+  imports: [InputFieldComponent, RouterLink, BackButtonComponent],
 })
 export class LoginComponent {
   step = 1;
   handleBack() {
-    // Pode navegar para outra página, fechar modal, etc.
     console.log('Botão de voltar clicado');
   }
   handleNext() {
