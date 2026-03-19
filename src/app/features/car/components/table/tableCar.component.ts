2605eeb3593f925fb05b67d60a33b24a5f369b7b
Nathália Almeida Amorum <nathaliaalmeida@prefeitura.sp.gov.br>
2026-03-19 12:12:04 -0300

    feat: adicionando modais nas tabelas

--- src/app/features/car/components/table/tableCar.component.ts
+++ src/app/features/car/components/table/tableCar.component.ts
@@ -4,17 +4,29 @@ import { TableHeaderComponent } from '../../../../shared/components/tableHeader/
 import { CommonModule } from '@angular/common';
 import { TableActionsComponent } from '../../../../shared/components/buttonTable/buttonTable.component';
 import { TableVehiclesMock } from '../../service/mock';
+import { ModalDelete } from '../../../../shared/components/modalDelete/modalDelete.component';
+import { ModalComponent } from '../../../../shared/components/popup/popup.component';
 
 @Component({
   selector: 'app-table-car',
   standalone: true,
   templateUrl: './tableCar.component.html',
   styleUrls: ['./tableCar.component.scss'],
-  imports: [CommonModule, TableHeaderComponent, TableFooterComponent, TableActionsComponent],
+  imports: [
+    CommonModule,
+    TableHeaderComponent,
+    TableFooterComponent,
+    TableActionsComponent,
+    ModalComponent,
+    ModalDelete,
+  ],
 })
 export class TableCar {
   page = 1;
   totalPages = 5;
+
+  activeModal: 'edit' | 'delete' | null = null;
+  selectedOs: any = null;
 
   columns = TableVehiclesMock.columns;
   vehicles = TableVehiclesMock.vehicles;
@@ -27,11 +39,23 @@ export class TableCar {
     this.page = newPage;
   }
 
-  handleEdit(client: any) {
-    console.log('Editar:', client);
+  handleEdit(os: any) {
+    this.selectedOs = os;
+    this.activeModal = 'edit';
   }
 
-  handleDelete(client: any) {
-    console.log('Deletar:', client);
+  handleDelete(os: any) {
+    this.selectedOs = os;
+    this.activeModal = 'delete';
+  }
+
+  confirmDelete() {
+    console.log('Deletar OS:', this.selectedOs);
+    this.closeModal();
+  }
+
+  closeModal() {
+    this.activeModal = null;
+    this.selectedOs = null;
   }
 }
