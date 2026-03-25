2f70a7efcfbe1f7b2bcd9ab49bd7db4e81350cbb
Nathália Almeida Amorum <nathaliaalmeida@prefeitura.sp.gov.br>
2026-03-25 13:15:48 -0300

    feat: config api finalizada

--- src/app/core/api/apiConfig.ts
+++ src/app/core/api/apiConfig.ts
@@ -1,7 +1,8 @@
 import axios from 'axios';
+import { environment } from '../../../environments/environments';
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL,
+  baseURL: environment.apiUrl,
   timeout: 5000,
   headers: {
     'Content-Type': 'application/json',
