9881cf9b013fa3280f6b68262c5c7867b69b47ce
Steffany Medeiros Lopes <steffanymedeiros@prefeitura.sp.gov.br>
2026-04-06 10:58:29 -0300

    feat: ajustes em icones

--- src/app/shared/components/tableHeader/tableHeader.component.ts
+++ src/app/shared/components/tableHeader/tableHeader.component.ts
@@ -1,14 +1,17 @@
+import { CommonModule } from '@angular/common';
 import { Component, Input, Output, EventEmitter } from '@angular/core';
 import { TableFooterComponent } from '../tableFooter/tableFooter.component';
 
 @Component({
   selector: 'app-table-header',
   standalone: true,
+  imports: [CommonModule],
   templateUrl: './tableHeader.component.html',
   styleUrls: ['./tableHeader.component.scss'],
 })
 export class TableHeaderComponent {
   @Input() icon!: string;
+  @Input() iconSrc?: string;
   @Input() title!: string;
 
   @Output() search = new EventEmitter<string>();
