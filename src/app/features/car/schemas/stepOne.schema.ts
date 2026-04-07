a8de1012d6e9c4971fd4db68d02443e2ec97f1a1
Nathália Almeida Amorum <nathaliaalmeida@prefeitura.sp.gov.br>
2026-04-07 14:23:03 -0300

    fix: schema placa padrao mercosul

--- src/app/features/car/schemas/stepOne.schema.ts
+++ src/app/features/car/schemas/stepOne.schema.ts
@@ -3,7 +3,10 @@ import * as yup from 'yup';
 export const stepOneSchema = yup.object({
   cliente: yup.number().nullable().required('Cliente é obrigatório'),
 
-  plate: yup.string().required('Placa é obrigatória').min(7, 'Placa inválida'),
+  plate: yup
+    .string()
+    .required('Placa é obrigatória')
+    .matches(/^[A-Z]{3}[0-9][A-Z][0-9]{2}$/, 'Placa inválida. Formato correto: ABC1D23'),
 
   year: yup
     .number()
